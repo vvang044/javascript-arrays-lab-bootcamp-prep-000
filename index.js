1187200bd7be15b08061b14b2a655377f6b366fd
Vik Vangala <vvang044@gmail.com>
2019-05-28 01:21:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var kittens = [""] //define your array here
+var kittens = ["eenie", "meanie", "miney"] //define your array here
 
 // Add your functions and code here
+ function removeFirstKitten(){
+   var newArray = kittens.
+ }
