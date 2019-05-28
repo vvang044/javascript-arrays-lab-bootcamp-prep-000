026420f5cfeaea8ed7ff8db7890e03dd693a2d15
Vik Vangala <vvang044@gmail.com>
2019-05-28 01:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var kittens = ["eenie", "meanie", "miney"] //define your array here
 
 // Add your functions and code here
  function removeFirstKitten(){
-   var newArray = kittens.
+   var newArray = kittens.unshift()
  }
